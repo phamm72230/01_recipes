119605f538584d913a594a0df3959d5da0ce2672
phamm72230 <54381277+phamm72230@users.noreply.github.com>
2020-02-20 09:38:50 +1300

    finds sf but does not check input

--- 01_recipe_name.py
+++ 01_recipe_name.py
@@ -2,12 +2,23 @@
 
 # Not blank function goes here
 def not_blank(question):
+    error = "Your recipe name has numbers in it."
 
     valid = False
     while not valid:
         response = input(question)
+        has_errors = ""
+
+        # look at each character in string and if it's a number, complain
+        for letter in response:
+            if letter.isdigit() == True:
+                has_errors = "yes"
+                break
 
         if response == "":
+            continue
+        elif has_errors != "":
+            print(error)
             continue
         else:
             return response
---
+++ 01b_no_numbers.py
@@ -0,0 +1,18 @@
+# Iterates through string...
+
+# ask user for string
+recipe_name = input("What is the recipe name? ")
+
+error = "Your recipe name has numbers in it."
+has_errors = ""
+
+# look at each character in string and if it's a number, complain
+for letter in recipe_name:
+    if letter.isdigit() == True:
+        print(error)
+        has_errors = "yes"
+        break
+
+# give user feedback...
+if has_errors != "yes":
+    print("you are okay")
